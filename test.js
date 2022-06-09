e182056e5c7d1b67cbb52739bcecdec24ecc82e6
Alan Hoffer <alanhoffer97@hotmail.com>
2022-06-08 21:02:09 -0300

    First Commit

---
+++ test.js
@@ -0,0 +1,15 @@
+let readlineSync = require('readline-sync');
+
+// Ejercicio
+
+let mensaje = "Hola Mundo"
+
+console.log(`Mensaje: ${mensaje}`);
+
+
+let base = readlineSync.question("Ingrese la base: ");
+let altura = readlineSync.question("Ingrese la altura: ");
+
+console.log(`Base: ${base} Altura: ${altura}`);
+
+
